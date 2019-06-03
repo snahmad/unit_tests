1e47b4a0eebed42bf469000f130d7735ba7fa57f
Naeem Ahmad <naeem.ahmad@actisense.com>
2019-06-03 16:33:39 +0100

    Add project files.

---
+++ msunit_github/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace msunit_github
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6, 5);
+        }
+    }
+}
